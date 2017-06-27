fd26f2fa18817c529973416c8d0abab4edd044df
peparys <peparys@gi-pc119.in.fhkn.de>
2017-06-27 09:55:20 +0200

    added mockito with spy

--- prog2-testat-parys-291349/src/test/java/prog2/testat/RehaKursTestMock.java
+++ prog2-testat-parys-291349/src/test/java/prog2/testat/RehaKursTestMock.java
@@ -1,33 +1,44 @@
 package prog2.testat;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
-public class RehaKursTest {
+public class RehaKursTestMock {
 
+	@Spy
 	private RehaKurs rehaKurs;
 
 	@Before
 	public void setup() {
-		rehaKurs = new RehaKurs();
+		MockitoAnnotations.initMocks(this);
 	}
 
 	@Test
 	public void testHinzufuegen() {
 		rehaKurs.hinzufuegen("Maxi", "Müller", 28);
 		rehaKurs.hinzufuegen("Hänsel", "Waldmann", 17);
-
+		
 		assertEquals("Maxi", rehaKurs.gibTeilnehmer(1).getVorname());
 		assertEquals("Müller", rehaKurs.gibTeilnehmer(1).getNachname());
 		assertEquals(28, rehaKurs.gibTeilnehmer(1).getAlter());
-
+		
 		assertEquals("Hänsel", rehaKurs.gibTeilnehmer(2).getVorname());
 		assertEquals("Waldmann", rehaKurs.gibTeilnehmer(2).getNachname());
 		assertEquals(17, rehaKurs.gibTeilnehmer(2).getAlter());
+		
+		InOrder orderRehaKurs = inOrder(rehaKurs);
+		orderRehaKurs.verify(rehaKurs).hinzufuegen("Maxi", "Müller", 28);
+		orderRehaKurs.verify(rehaKurs).hinzufuegen("Hänsel", "Waldmann", 17);
+		verify(rehaKurs, never()).unter18();
+		
 	}
 
 	@Test(expected = IllegalArgumentException.class)
@@ -48,7 +59,7 @@ public class RehaKursTest {
 		assertEquals(1, rehaKurs.unter18());
 	}
 
-	@Test
+	@Test 
 	public void testTeilnehmerMinMaxAlter() {
 		rehaKurs.hinzufuegen("Maxi", "Müller", 28);
 		rehaKurs.hinzufuegen("Hänsel", "Waldmann", 17);
@@ -60,6 +71,8 @@ public class RehaKursTest {
 		assertEquals("Maxi", teilnehmerListeMinMaxAlter.get(0).getVorname());
 		assertEquals("Müller", teilnehmerListeMinMaxAlter.get(0).getNachname());
 		assertEquals(28, teilnehmerListeMinMaxAlter.get(0).getAlter());
+		
+		verify(rehaKurs, times(1)).getTeilnehmerByAlter(anyInt(), anyInt());
 
 	}
 
